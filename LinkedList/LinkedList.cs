964cee6dc9d548f025b0ae3efb72e331ec739848
Rokko1egend <elliot.jost@gmail.com>
2025-03-27 18:43:59 -0600

    Add project files.

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * Title:LinkedListLab
+ * Author:Elliot Jost
+ * Date: 2025-03-27
+ */
+
+namespace LinkedListSpace
+{
+    public class LinkedList
+    {
+        private class Node
+        {
+            public string Value;
+            public Node Next;
+            public Node(string value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private int count;
+        private Node head;
+
+        public int Count => count;
+        public string Head => head?.Value;
+
+        public void AddFirst(string value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = head;
+            head = newNode;
+            count += 1;
+        }
+
+        public void AddLast(string value)
+        {
+            Node newNode = new Node(value);
+
+            if (head == null)
+            {
+                head = newNode;
+
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+
+            }
+            count += 1;
+        }
+        public void RemoveFirst()
+        {
+            if (head != null)
+            {
+                head = head.Next;
+                count -= 1;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (head == null)
+            {
+                return;
+            }
+            else if (head.Next == null)
+            {
+                head = null;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+            }
+            count -= 1;
+        }
+
+        public string GetValue(int index)
+        {
+            if ((index < 0) || (index >= count))
+            {
+                throw new IndexOutOfRangeException("Index Out of Bounds");
+            }
+
+            Node current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.Value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            
+            list.AddFirst("First");
+            list.AddLast("Middle");
+            list.AddLast("Last");
+            list.RemoveLast();
+            list.RemoveFirst();
+
+            Console.WriteLine(list.GetValue(0));
+
+        }
+    }
+}
---
+++ LinkedListTest/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+using LinkedListSpace;
+using NUnit.Framework;
+
+namespace LinkedListTests
+{
+    [TestFixture] // Says this class contains tests
+    public class LinkedListTests
+    {
+        public LinkedList? list; //Preparing object
+
+        [SetUp] // This meathod is executed before each test run
+        public void Setup()
+        {
+            list = new LinkedList();
+        }
+
+        [TearDown] // This methode is ran after every test run
+        public void TearDown()
+        {
+            list = null;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass(); // always passes
+        }
+
+        //testing Every meathod from "Linked List"
+        [Test]
+        public void Test_AddFirst()
+        {
+
+            list!.AddFirst("Joe Blow");
+            list!.AddFirst("Joe Schmoe");
+            list!.AddFirst("John Smith");
+            list!.AddFirst("Jane Doe");
+
+            Assert.That("Jane Doe", Is.EqualTo(list!.GetValue(0)));
+        }
+
+        [Test]
+        public void Test_AddLast()
+        {
+            list!.AddLast("Bob Bobberson");
+            list!.AddLast("Sam Sammerson");
+            list!.AddLast("Dave Daverson");
+
+            Assert.That("Dave Daverson", Is.EqualTo(list!.GetValue(2)));
+        }
+
+        [Test]
+        public void Test_RemoveFirst()
+        {
+            list!.AddLast("Bob Bobberson");
+            list!.AddLast("Sam Sammerson");
+            list!.AddLast("Dave Daverson");
+
+            list!.RemoveFirst();
+            list!.RemoveFirst();
+            System.Console.WriteLine(list!.GetValue(0));
+            Assert.That("Dave Daverson", Is.EqualTo(list!.GetValue(0)));
+        }
+
+        [Test]
+        public void Test_RemoveLast()
+        {
+            list!.AddLast("Bob Bobberson");
+            list!.AddLast("Sam Sammerson");
+            list!.AddLast("Dave Daverson");
+            list!.RemoveLast();
+            list!.RemoveLast();
+
+            Assert.That("Bob Bobberson", Is.EqualTo(list!.GetValue(0)));
+        }
+
+        [Test]
+        public void Test_GetValue()
+        {
+            list!.RemoveLast();
+            list!.RemoveLast();
+            list!.AddFirst("Sam Sammerson");
+            Assert.That("Sam Sammerson", Is.EqualTo(list!.GetValue(0)));
+        }
+
+        //Tests the size of the list
+        [Test]
+        public void Test_GetListSize()
+        {
+            list!.AddFirst("Joe Blow");
+            list!.AddFirst("Joe Schmoe");
+            list!.AddFirst("John Smith");
+
+            Assert.That(3, Is.EqualTo(list!.Count));
+        }
+    }
+}
